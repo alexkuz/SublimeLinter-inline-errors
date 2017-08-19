3a1f1d9b1d3e0476f3b1fc5e5e42768a6d6e3f16
Kuzya <alexkuz@gmail.com>
2017-08-19 10:45:52 +0300

    render errors and warning separately; bug fixes

--- inline-errors.py
+++ inline-errors.py
@@ -1,31 +1,66 @@
 import re
+from cgi import escape
 from string import Template
 import textwrap
 import sublime
 import sublime_plugin
 from SublimeLinter.sublimelinter import SublimeLinter as Linter
-from SublimeLinter.lint import persist
+from SublimeLinter.lint import persist, highlight
+
+DEBUG = None
 
 PHANTOM_SETS_BY_BUFFER = {}
 
 
-def settings():
-    return sublime.load_settings('SublimeLinterInlineErrors.sublime-settings')
+class InlineErrorSettings:
+    inline_theme = None
+    below_theme = None
+    hint_on_selected_line = None
+    min_offset = None
+    max_block_width = None
+    min_gap = None
+    show_inline_text = None
+    warning_symbol = None
+    error_symbol = None
+    offset_symbol = None
+    offset_color = None
+    inline_warning_color = None
+    inline_warning_background_color = None
+    inline_error_color = None
+    inline_error_background_color = None
+    below_warning_color = None
+    below_warning_background_color = None
+    below_error_color = None
+    below_error_background_color = None
+    inline_max_words = None
+    font_size = None
+    debug = None
+
+    def __init__(self):
+        s = sublime.load_settings('SublimeLinterInlineErrors.sublime-settings')
+        fields = [f for f in dir(self) if not f.startswith('__')]
+        for f in fields:
+            setattr(self, f, s.get(f))
 
 
 def print_debug(*args):
-    if settings().get('debug'):
+    global DEBUG
+    if DEBUG is None:
+        DEBUG = InlineErrorSettings().debug
+    if DEBUG:
         'invalid syntax; SyntaxError'
         print('[INLINE ERRORS]', *args)
 
 
 def plugin_loaded():
+    global PHANTOM_SETS_BY_BUFFER
     print_debug('Clear all phantoms')
     for _, phantom_set in PHANTOM_SETS_BY_BUFFER.items():
         phantom_set.update([])
 
 
 def plugin_unloaded():
+    global PHANTOM_SETS_BY_BUFFER
     print_debug('Clear all phantoms')
     for _, phantom_set in PHANTOM_SETS_BY_BUFFER.items():
         phantom_set.update([])
@@ -34,6 +69,12 @@ def plugin_unloaded():
 class InlineErrors(sublime_plugin.ViewEventListener):
     _expanded_error_line = None
     linter = None
+    _settings = None
+
+    def settings(self):
+        if self._settings is None:
+            self._settings = InlineErrorSettings()
+        return self._settings
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -57,8 +98,11 @@ class InlineErrors(sublime_plugin.ViewEventListener):
             return res
         Linter.clear = _clear
 
-    def get_template(self, theme_name):
-        theme_path = settings().get(theme_name)
+    def get_template(self, layout):
+        if layout == sublime.LAYOUT_INLINE:
+            theme_path = self.settings().inline_theme
+        elif layout == sublime.LAYOUT_BELOW:
+            theme_path = self.settings().below_theme
 
         if theme_path == 'none' or theme_path is None:
             return False
@@ -86,9 +130,10 @@ class InlineErrors(sublime_plugin.ViewEventListener):
         if vid in persist.errors:
             errors = persist.errors[vid]
 
-            self.show_phantom(view, errors, lineno)
+            self.show_phantom(view, errors, lineno, persist.highlights[vid].all)
 
     def get_phantom_set(self, view):
+        global PHANTOM_SETS_BY_BUFFER
         buffer_id = view.buffer_id()
         if buffer_id not in PHANTOM_SETS_BY_BUFFER:
             phantom_set = sublime.PhantomSet(view, 'linter-inline-errors')
@@ -98,10 +143,10 @@ class InlineErrors(sublime_plugin.ViewEventListener):
 
         return phantom_set
 
-    def show_phantom(self, view, errors, selected_line):
+    def show_phantom(self, view, errors, selected_line, highlights):
         templates = {
-            'inline': self.get_template('inline_theme'),
-            'below': self.get_template('below_theme')
+            'inline': self.get_template(sublime.LAYOUT_INLINE),
+            'below': self.get_template(sublime.LAYOUT_BELOW)
         }
 
         if not templates['inline'] or not templates['below']:
@@ -110,63 +155,59 @@ class InlineErrors(sublime_plugin.ViewEventListener):
         phantom_set = self.get_phantom_set(view)
 
         phantoms = [
-            self.get_phantoms(line, errs, templates, view, selected_line == line)
+            self.get_phantoms(line, errs, templates, view, selected_line == line, highlights)
             for line, errs in errors.items()
         ]
         print_debug('Update phantoms: %s' % len(phantoms))
         phantom_set.update([p for pair in phantoms for p in pair if p])
 
     def clear(self, view):
-        phantom_set = self.get_phantom_set(view)
         print_debug('Clear phantoms')
-        phantom_set.update([])
+        view.erase_phantoms('linter-inline-errors')
 
-    def get_phantoms(self, line, errors, templates, view, is_selected):
-        hint_on_selected_line = settings().get('hint_on_selected_line')
+    def is_error(self, row, col, highlights):
+        pos = self.view.text_point(row, col)
+        for h in highlights:
+            if len([e for e in h.marks['error'] if e.a == pos]) > 0:
+                return True
+
+        return False
+
+    def get_phantoms(self, line, errors, templates, view, is_selected, highlights):
 
         line_errors = sorted(errors, key=lambda error: error[0])
-        line_errors = [error[1] for error in line_errors]
+        line_errors = [(error[1], self.is_error(line, error[0], highlights)) for error in line_errors]
 
-        s = settings()
-        min_offset = s.get('min_offset')
-        max_block_width = s.get('max_block_width')
-        min_gap = s.get('min_gap')
-        show_inline_text = s.get('show_inline_text')
-        warning_symbol = s.get('warning_symbol')
-        offset_symbol = s.get('offset_symbol')
-        offset_color = s.get('offset_color')
-        inline_hint_color = s.get('inline_hint_color')
-        inline_hint_background_color = s.get('inline_hint_background_color')
-        below_hint_color = s.get('below_hint_color')
-        below_hint_background_color = s.get('below_hint_background_color')
-        inline_max_words = s.get('inline_max_words')
-        font_size = s.get('font_size')
+        s = self.settings()
 
         region = view.line(view.text_point(line, 0))
         line_width = region.b - region.a
-        left_offset = max(min_offset - line_width, min_gap)
-        is_expanded = line == self._expanded_error_line or hint_on_selected_line == 'below' and is_selected
+        left_offset = max(s.min_offset - line_width, s.min_gap)
+        is_expanded = line == self._expanded_error_line or s.hint_on_selected_line == 'below' and is_selected
 
-        if hint_on_selected_line == 'none' and is_selected and not is_expanded:
+        if s.hint_on_selected_line == 'none' and is_selected and not is_expanded:
             return (None, None)
 
-        def wrap_line(line):
-            wrapped = textwrap.wrap(line, max_block_width, break_long_words=False)
-            return [
-                ('%s %s' % (warning_symbol, l)
-                 if idx == 0 else '<div class="pad">%s</div>' % l)
+        def wrap_line(line, is_error):
+            hint_symbol = s.error_symbol if is_error else s.warning_symbol
+            wrapped = textwrap.wrap(line, s.max_block_width, break_long_words=False)
+            lines = [
+                ('%s %s' % (hint_symbol, escape(l))
+                 if idx == 0 else '<div class="pad">%s</div>' % escape(l))
                 for (idx, l) in enumerate(wrapped)
             ]
 
-        has_inline_text = show_inline_text and not is_expanded and (
-            not is_selected or hint_on_selected_line == 'inline'
-        )
-        inline_text = '; '.join(line_errors)
+            return lines
 
-        if inline_max_words:
+        has_inline_text = s.show_inline_text and not is_expanded and (
+            not is_selected or s.hint_on_selected_line == 'inline'
+        )
+        inline_text = '; '.join([l[0] for l in line_errors])
+
+        if s.inline_max_words:
             inline_text_words = inline_text.split(' ')
-            if len(inline_text_words) > inline_max_words:
-                inline_text = '%s…' % ' '.join(inline_text_words[:inline_max_words])
+            if len(inline_text_words) > s.inline_max_words:
+                inline_text = '%s…' % ' '.join(inline_text_words[:s.inline_max_words])
 
         viewport_width = int(self.view.viewport_extent()[0] / self.view.em_width()) - 3
 
@@ -176,13 +217,21 @@ class InlineErrors(sublime_plugin.ViewEventListener):
 
         if is_selected:
             hint_overflow = (region.b - region.a) + left_offset + len(inline_text) - viewport_width + 4
-            print((region.b - region.a) + left_offset + len(inline_text), viewport_width)
             if hint_overflow > 0:
                 fixed_width = len(inline_text) - hint_overflow - 1
                 inline_text = '%s…' % inline_text[:fixed_width] if fixed_width > 0 else ''
 
-        inline_message = '<a href="%s">%s %s</a>' % (line, warning_symbol, inline_text) if has_inline_text else ''
-        below_message = ''.join(['<a href="%s">%s</a>' % (line, l) for e in line_errors for l in wrap_line(e)])
+        has_error = len([is_error for (l, is_error) in line_errors if is_error]) > 0
+        hint_symbol = s.error_symbol if has_error else s.warning_symbol
+        classname = 'inline_error' if has_error else 'inline_warning'
+        inline_message = (
+            '<a href="%s" class="%s">%s %s</a>' % (line, classname, hint_symbol, escape(inline_text))
+            if has_inline_text else ''
+        )
+        below_message = ''.join([
+            '<a href="%s" class="%s">%s</a>' % (line, 'below_error' if is_error else 'below_warning', l)
+            for (lines, is_error) in line_errors for l in wrap_line(lines, is_error)
+        ])
 
         line_text = view.substr(region)
         match = re.search(r'[^\s]', line_text)
@@ -190,27 +239,37 @@ class InlineErrors(sublime_plugin.ViewEventListener):
 
         inline_tooltip_content = templates['inline'].substitute(
             line=line,
-            left_offset='<div class="offset"> %s </div>' % (offset_symbol * left_offset),
-            message='<a href="%s">%s</a>' % (line, warning_symbol) if not has_inline_text else inline_message,
-            font_size=font_size,
-            offset_color=offset_color,
-            hint_background_color=(
-                'background-color: #%s;' % inline_hint_background_color
-                if inline_hint_background_color else ''
+            left_offset='<div class="offset"> %s </div>' % (s.offset_symbol * left_offset),
+            message='<a href="%s">%s</a>' % (line, hint_symbol) if not has_inline_text else inline_message,
+            font_size=s.font_size,
+            offset_color=s.offset_color,
+            warning_background_color=(
+                'background-color: #%s;' % s.inline_warning_background_color
+                if s.inline_warning_background_color else ''
             ),
-            hint_color='color: #%s;' % inline_hint_color if inline_hint_color else ''
+            warning_color='color: #%s;' % s.inline_warning_color if s.inline_warning_color else '',
+            error_background_color=(
+                'background-color: #%s;' % s.inline_error_background_color
+                if s.inline_error_background_color else ''
+            ),
+            error_color='color: #%s;' % s.inline_error_color if s.inline_error_color else ''
         )
 
         below_tooltip_content = templates['below'].substitute(
             line=line,
             left_offset='',
             message=below_message,
-            font_size=font_size,
-            hint_background_color=(
-                'background-color: #%s;' % below_hint_background_color
-                if below_hint_background_color else ''
+            font_size=s.font_size,
+            warning_background_color=(
+                'background-color: #%s;' % s.below_warning_background_color
+                if s.below_warning_background_color else ''
             ),
-            hint_color='color: #%s;' % below_hint_color if below_hint_color else ''
+            error_background_color=(
+                'background-color: #%s;' % s.below_error_background_color
+                if s.below_error_background_color else ''
+            ),
+            warning_color='color: #%s;' % s.below_warning_color if s.below_warning_color else '',
+            error_color='color: #%s;' % s.below_error_color if s.below_error_color else ''
         )
 
         inline_phantom = sublime.Phantom(
